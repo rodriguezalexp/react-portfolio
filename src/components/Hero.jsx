d65e5f54e21c0544ae8c437c2b23fb1385795406
rodriguezalexp <alexander.0894-rp@hotmail.com>
2024-08-27 18:59:45 -0500

    new push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
  import {HERO_CONTENT} from "../constants"
- import profilePic from "../assets/kevinRushProfile.jpg"
+ import profilePic from "../assets/AlexanderRodriguez.jpeg"
  import { motion } from "framer-motion"
  
 const container = (delay) => ({
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,13 @@
 import { PROJECTS } from "../constants"
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <h1 className="my-20 text-center text-4xl">Projects</h1>
+        <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial= {{opacity: 0, y: -100}}
+        className="my-20 text-center text-4xl">Projects</motion.h1>
         <div>
             {PROJECTS.map((project, index) => (
                 <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
