dad1d2ccfb8671ef1f47cf78c6ea1849e7d89b45
rodriguezalexp <alexander.0894-rp@hotmail.com>
2024-08-27 11:51:42 -0500

    framer motion lib installed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
  import {HERO_CONTENT} from "../constants"
  import profilePic from "../assets/kevinRushProfile.jpg"
+ import { motion } from "framer-motion"
  
  const Hero = () => {
   return (
@@ -7,7 +8,8 @@
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
             <div className="flex flex-col items-center lg:items-start">
-                <h1>Alexander Rodriguez</h1>
+                <motion.h1 animate={{ x: 100 }} className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                  Alexander Rodriguez Pineda </motion.h1>
                 <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"> Full Stack Developer</span>
                 <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
             </div>
