c4c092953415b0d14f42b044a2c6469f274d730c
rodriguezalexp <alexander.0894-rp@hotmail.com>
2024-09-03 17:53:44 -0500

    update Hero section

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,17 +3,23 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `Soy una desarrollador full-stack versatil, dedicado y con gran interes en crear aplicaciones web amigables con el usuario. cuento con dos años de experiencia en el mundo del desarrollo, he trabajado con una gran cantidad de tecnologias entre las cuales destacan Python, NodeJs, React, Django, MongoDb, PostgreSql.`
+export const HERO_CONTENT = `Soy un desarrollador full-stack versátil, dedicado y con gran interés en crear aplicaciones web amigables con el usuario. Cuento con dos años de experiencia en el mundo del desarrollo. He trabajado con una gran cantidad de tecnologías, entre las cuales destacan Python, NodeJs, React, Django, MongoDb y PostgreSql. Mi meta es aprovechar mis conocimientos para crear experiencias amigables y útiles que puedan servir a las personas o empresas.`
 
 /* `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`; */
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `Desde que tengo memoria, he estado interesado en descubrir cómo funcionan las cosas. Esa misma curiosidad me llevó a adentrarme en el mundo del software y, desde entonces, no he parado de estudiar las diferentes tecnologías que se pueden utilizar para brindar soluciones a quien lo necesite.
 
-/* `Soy una desarrollador full-stack versatil, dedicado y con gran interes en crear aplicaciones web amigables con el usuario. cuento con dos años de experiencia en el mundo del desarrollo, he trabajado con una gran cantidad de tecnologias entre las cuales destacan Python, NodeJs, React, Django, MongoDb, PostgreSql. My meta como persona y profesional ha sido crear soluciones que puedan ayudar a las personas a lograr sus m
+Mi lenguaje principal es Python, pero cuento con gran adaptabilidad para nuevos aprendizajes de lenguajes o frameworks. Algunas de las tecnologías que he utilizado son Python, NodeJs, React, Django, MongoDb y PostgreSql.
 
+Aunque tengo bases sólidas y he realizado varios proyectos funcionales y colaborativos, considero que tengo mucho que aprender. Por eso, si usted que está leyendo esto es un reclutador y está buscando a alguien que tenga conocimientos y que, además, quiera aprender, por favor no dude en tenerme en consideración.`;
 
+/* 
+Desde que tengo memoria he estado interesado en descubrir como funcionan las cosas, esa misma curiosidad me llevo a adentrarme en el mundo del software y desde entonces no he parado de estudiar las diferentes tecnologias que se pueden utilizar para brindar soluciones al que lo necesite.
+Mi lenguaje principal es Python pero cuento con gran adaptabilidad para nuevos aprendizajes de lenguajes o frameworks, algunas de las tecnologias que he utilizado son Python, NodeJs, React, Django, MongoDb y PostgreSql.
 
-Desde que tengo memoria he estado interesado en descubrir como funcionan las cosas, esa misma curiosidad me llevo a estudiar desarrollo de software, una carrera que ` */
+Aunque tengo bases solidas y he realizado varios proyectos funcionales y colaborativos, considero que tengo mucho que aprender, por eso, si usted que esta leyendo esto es un reclutador y esta buscando alguien que tenga conocimientos y que ademas quiera aprender, por favor no dude en tenerme en consideracion.
+
+` */
 
 export const EXPERIENCES = [
   {
