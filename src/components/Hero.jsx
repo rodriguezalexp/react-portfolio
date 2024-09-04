3e804e342d38236acf9d7bd9ab5104f2fe03240a
rodriguezalexp <alexander.0894-rp@hotmail.com>
2024-09-03 20:59:14 -0500

    adding complete name and logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const container = (delay) => ({
                 initial="hidden"
                 animate="visible"
                 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
-                  Alexander Rodriguez</motion.h1>
+                  Alexander Rodriguez Pineda</motion.h1>
                 <motion.span
                 variants={container(0.5)}
                 initial="hidden"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import logo from "../assets/kevinRushLogo.png"
+import logo from "../assets/Ar1.png"
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
-import { FaSquareXTwitter} from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
+/* import { FaSquareXTwitter} from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6"; */
 
 const Navbar = () => {
   return (
@@ -11,10 +11,18 @@ const Navbar = () => {
             <img className="mx-2 w-10" src={logo} alt="logo" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <a href="https://www.linkedin.com/in/rodriguezalexp/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin />
+            </a>
+            <a href="https://github.com/rodriguezalexp" target="_blank" rel="noopener noreferrer">
             <FaGithub />
+            </a>
+            {/* <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
             <FaSquareXTwitter/>
+            </a> */}
+            {/* <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
             <FaInstagram />
+            </a> */}
         </div>
     </nav>
   );
